ab6b2ce77e3a648ed71810d175c5056e9e0ef240
Малик <93332098+Malikk-Sh@users.noreply.github.com>
2022-05-27 19:10:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,197 @@
+let game = document.getElementById('game');
+
+let cell = '<div class="cell"></div>';
+
+for (let i = 0; i < 9; i++) {
+    game.innerHTML += cell;
+};
+
+let hod = 2;
+
+let cells = document.getElementsByClassName('cell');
+
+let wPs = document.querySelectorAll('.pos');
+
+let winAlert = document.querySelector('.win-alert');
+
+let reload = document.querySelector('.reload');
+
+let hasWin = false
+
+reload.onclick = () => {
+    location.reload();
+    return false
+}
+
+ let doing = function(e) {
+    if (!e.target.classList.contains('cell')) return;
+    if (e.target.innerHTML) {
+        return;
+    } else {
+        hod++;
+    }
+    // console.log(e.target);
+    if (hod %= 2) {
+        e.target.innerHTML = '×';
+    }
+    else {
+        e.target.innerHTML = '𐌏';
+    }
+    if (cells[0].innerHTML == '𐌏' &
+        cells[1].innerHTML == '𐌏' &
+        cells[2].innerHTML == '𐌏'
+    ) {
+        winO();
+        wPs[0].style.display = 'block';
+    };
+    if (cells[3].innerHTML == '𐌏' &
+        cells[4].innerHTML == '𐌏' &
+        cells[5].innerHTML == '𐌏'
+    ) {
+        winO();
+        wPs[1].style.display = 'block';
+    };
+    if (cells[6].innerHTML == '𐌏' &
+        cells[7].innerHTML == '𐌏' &
+        cells[8].innerHTML == '𐌏'
+    ) {
+        winO();
+        wPs[2].style.display = 'block';
+    };
+    if (cells[0].innerHTML == '𐌏' &
+        cells[4].innerHTML == '𐌏' &
+        cells[8].innerHTML == '𐌏'
+    ) {
+        winO();
+        wPs[7].style.display = 'block';
+    };
+    if (cells[2].innerHTML == '𐌏' &
+        cells[4].innerHTML == '𐌏' &
+        cells[6].innerHTML == '𐌏'
+    ) {
+        winO();
+        wPs[6].style.display = 'block';
+    };
+    if (cells[0].innerHTML == '𐌏' &
+        cells[3].innerHTML == '𐌏' &
+        cells[6].innerHTML == '𐌏'
+    ) {
+        winO();
+        wPs[3].style.display = 'block';
+    };
+    if (cells[1].innerHTML == '𐌏' &
+        cells[4].innerHTML == '𐌏' &
+        cells[7].innerHTML == '𐌏'
+    ) {
+        winO();
+        wPs[4].style.display = 'block';
+    };
+    if (cells[2].innerHTML == '𐌏' &
+        cells[5].innerHTML == '𐌏' &
+        cells[8].innerHTML == '𐌏'
+    ) {
+        winO();
+        wPs[5].style.display = 'block';
+    };
+
+
+
+
+    if (cells[0].innerHTML == '×' &
+        cells[1].innerHTML == '×' &
+        cells[2].innerHTML == '×'
+    ) {
+        winX();
+        wPs[0].style.display = 'block';
+    };
+    if (cells[3].innerHTML == '×' &
+        cells[4].innerHTML == '×' &
+        cells[5].innerHTML == '×'
+    ) {
+        winX();
+        wPs[1].style.display = 'block';
+    };
+    if (cells[6].innerHTML == '×' &
+        cells[7].innerHTML == '×' &
+        cells[8].innerHTML == '×'
+    ) {
+        winX();
+        wPs[2].style.display = 'block';
+    };
+    if (cells[0].innerHTML == '×' &
+        cells[4].innerHTML == '×' &
+        cells[8].innerHTML == '×'
+    ) {
+        winX();
+        wPs[7].style.display = 'block';
+    };
+    if (cells[2].innerHTML == '×' &
+        cells[4].innerHTML == '×' &
+        cells[6].innerHTML == '×'
+    ) {
+        winX();
+        wPs[6].style.display = 'block';
+    };
+    if (cells[0].innerHTML == '×' &
+        cells[3].innerHTML == '×' &
+        cells[6].innerHTML == '×'
+    ) {
+        winX();
+        wPs[3].style.display = 'block';
+    };
+    if (cells[1].innerHTML == '×' &
+        cells[4].innerHTML == '×' &
+        cells[7].innerHTML == '×'
+    ) {
+        winX();
+        wPs[4].style.display = 'block';
+    };
+    if (cells[2].innerHTML == '×' &
+        cells[5].innerHTML == '×' &
+        cells[8].innerHTML == '×'
+    ) {
+        winX();
+        wPs[5].style.display = 'block';
+    } else if (
+        (cells[0].innerHTML == '×' || cells[0].innerHTML == '𐌏') &
+        (cells[1].innerHTML == '×' || cells[1].innerHTML == '𐌏') &
+        (cells[2].innerHTML == '×' || cells[2].innerHTML == '𐌏') &
+        (cells[3].innerHTML == '×' || cells[3].innerHTML == '𐌏') &
+        (cells[4].innerHTML == '×' || cells[4].innerHTML == '𐌏') &
+        (cells[5].innerHTML == '×' || cells[5].innerHTML == '𐌏') &
+        (cells[6].innerHTML == '×' || cells[6].innerHTML == '𐌏') &
+        (cells[7].innerHTML == '×' || cells[7].innerHTML == '𐌏') &
+        (cells[8].innerHTML == '×' || cells[8].innerHTML == '𐌏') 
+        ){
+            if (!hasWin){
+                winAlert.style.display = "block"
+                winAlert.innerHTML = 'Ничья!';
+            }
+        };
+}
+window.addEventListener('click', doing);
+
+let xx = document.getElementById("xx");
+
+let oo = document.getElementById("oo");
+
+winAlert.style.display = "none";
+xx.style.display = "none";
+oo.style.display = "none";
+
+function winX() {
+    hasWin = true
+    setTimeout(() => {
+        winAlert.style.display = "block";
+        xx.style.display = "inline"
+    }, 50);
+    window.removeEventListener('click', doing);
+}
+function winO() {
+    hasWin = true
+    setTimeout(() => {
+        winAlert.style.display = "block";
+        oo.style.display = "inline";
+    }, 50);
+    window.removeEventListener('click', doing);
+}
